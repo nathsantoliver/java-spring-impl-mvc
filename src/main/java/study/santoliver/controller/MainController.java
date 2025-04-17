5c934ab4ca15175e5e7ab9618a2fe14854d9c401
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-04-17 12:28:11 -0300

    add a name parameter

--- src/main/java/study/santoliver/controller/MainController.java
+++ src/main/java/study/santoliver/controller/MainController.java
@@ -9,8 +9,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class MainController {
 
     @RequestMapping("/home")
-    public String home(@RequestParam String color, Model page) {
-        page.addAttribute("username", "Katy");
+    public String home(@RequestParam(required = false) String name,
+                       @RequestParam(required = false) String color, Model page) {
+        page.addAttribute("username", name);
         page.addAttribute("color", color);
 
         return "home.html";
