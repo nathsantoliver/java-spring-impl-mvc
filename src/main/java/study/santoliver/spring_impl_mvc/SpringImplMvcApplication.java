3bf89a9574d8a019064245c4bef3136424c3be20
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-04-17 10:37:04 -0300

    first commit

---
+++ src/main/java/study/santoliver/spring_impl_mvc/SpringImplMvcApplication.java
@@ -0,0 +1,13 @@
+package study.santoliver.spring_impl_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringImplMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringImplMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/santoliver/spring_impl_mvc/SpringImplMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package study.santoliver.spring_impl_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringImplMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
